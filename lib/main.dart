83aeba849fa7ba528431d89772f156315ae3946f
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2020-11-09 15:24:40 +0530

    -second commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
         backgroundColor: Colors.green,
         appBar: AppBar(
           title: Text('I Am Poor'),
-          backgroundColor: Colors.green[900],
+          backgroundColor: Colors.green[800],
         ),
         body: Center(
           child: Image(image: AssetImage('./images/homeless.jpg')),
